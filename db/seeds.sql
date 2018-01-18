caf7052ed105d624fd64dac384961d9798b43b79
boronflex <kyle_palmer@icloud.com>
2018-01-18 11:48:23 -0600

    copied from burger

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Royale with cheese');
+INSERT INTO burgers (burger_name) VALUES ('Cali Burger');
+INSERT INTO burgers (burger_name) VALUES ('THE CHALLA-NGE');
